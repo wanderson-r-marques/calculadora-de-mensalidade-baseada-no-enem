fd355b586cd3c9f9f8518b24d841e6d00ef79879
Wanderson R Marques <wanderson.r.marques@gmail.com>
2021-03-30 13:13:34 -0300

    Acrescentado o favicon

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
 <!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
 <head>
         <meta http-equiv="Cache-control" charset="utf-8">
-        <title>Calculadora</title>
-        <meta name="description" content="Ambiente de simulação de vendas de planos">
+        <title>Calculadora FB</title>
+        <link rel="icon" type="image/png" sizes="16x11" href="images/favicon.png">
+        <meta name="description" content="Ambiente de simulação de valor de mensalidade">
         <meta name="author" content="Proesc.com">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=yes">
         <link media='all' type='text/css' rel='stylesheet' href='css/bootstrap.min.css'>
